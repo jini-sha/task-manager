885e6ed4a4bec99e863c2cf4da7864df35da0fc5
Jinisha <jinishashr7@gmail.com>
2025-06-29 13:09:54 +0545

    Fixed error handling

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ const app=express();
 app.use(express.json());
 const taskRoutes=require('./routes/task.routes');
 app.use('/tasks',taskRoutes);
+
+const errorMiddleware = require('./middlewares/error.middleware');
+app.use(errorMiddleware);
 module.exports=app;
